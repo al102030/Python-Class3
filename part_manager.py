144347243ff1fbb5c1a921ded90ea80b92ee2ea6
Ali Darvishi <ali.darvishi.k@gmail.com>
2025-03-13 20:03:53 +0330

    End of TK

--- part_manager.py
+++ part_manager.py
@@ -1,24 +1,61 @@
 import tkinter as tk
+from tkinter import END, messagebox
+from db import Database
+
+db = Database('store.db')
 
 
 def populate_list():
-    print("Populate")
+    parts_list.delete(0, END)
+    for row in db.fetch():
+        parts_list.insert(END, row)
 
 
 def add_item():
-    print("Add")
+    if part_text.get() == '' or customer_text.get() == '' or retailer_text.get() == '' or price_text.get() == '':
+        messagebox.showerror('Required Fields', 'Please include all fields!')
+        return
+    db.insert(part_text.get(), customer_text.get(),
+              retailer_text.get(), price_text.get())
+    parts_list.delete(0, END)
+    parts_list.insert(END, (part_text.get(), customer_text.get(),
+                            retailer_text.get(), price_text.get()))
+    clear_text()
+    populate_list()
+
+
+def select_item(event):
+    global selected_item
+    index = parts_list.curselection()[0]
+    selected_item = parts_list.get(index)
+
+    part_entry.delete(0, END)
+    part_entry.insert(END, selected_item[1])
+    customer_entry.delete(0, END)
+    customer_entry.insert(END, selected_item[2])
+    retailer_entry.delete(0, END)
+    retailer_entry.insert(END, selected_item[3])
+    price_entry.delete(0, END)
+    price_entry.insert(END, selected_item[4])
 
 
 def remove_item():
-    print("Remove")
+    db.remove(selected_item[0])
+    clear_text()
+    populate_list()
 
 
 def update_item():
-    print("Update")
+    db.update(selected_item[0], part_text.get(), customer_text.get(),
+              retailer_text.get(), price_text.get())
+    populate_list()
 
 
 def clear_text():
-    print("Clear")
+    part_entry.delete(0, END)
+    customer_entry.delete(0, END)
+    retailer_entry.delete(0, END)
+    price_entry.delete(0, END)
 
 
 # tk._test()
@@ -67,6 +104,8 @@ scrollbar.grid(row=3, column=3, rowspan=6, pady=20, sticky='WNS')
 parts_list.configure(yscrollcommand=scrollbar.set)
 scrollbar.configure(command=parts_list.yview)
 
+# Bind select
+parts_list.bind('<<ListboxSelect>>', select_item)
 
 # Buttons
 add_btn = tk.Button(app, text='Add Part', width=12, command=add_item)
@@ -86,7 +125,8 @@ app. geometry('700x350')
 app.title("Simple App")
 
 populate_list()
-
+# populate_list()
+# populate_list()
 
 # Start Program
 app.mainloop()
